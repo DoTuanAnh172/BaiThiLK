84599b1c3bc8b56118bda62eadc1a713bb9fe390
DoTuanAnh172 <k205480106002@tnut.edu.vn>
2023-06-03 23:14:00 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace BaiThiLK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Ten_lop = new System.Windows.Forms.TextBox();
+            this.Ma_lop = new System.Windows.Forms.TextBox();
+            this.Ten_sinh_vien = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MSSV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(794, 448);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.Ten_lop);
+            this.tabPage1.Controls.Add(this.Ma_lop);
+            this.tabPage1.Controls.Add(this.Ten_sinh_vien);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.MSSV);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(786, 419);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(214, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Sửa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(368, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ten_lop
+            // 
+            this.Ten_lop.Location = new System.Drawing.Point(116, 131);
+            this.Ten_lop.Multiline = true;
+            this.Ten_lop.Name = "Ten_lop";
+            this.Ten_lop.Size = new System.Drawing.Size(250, 27);
+            this.Ten_lop.TabIndex = 8;
+            // 
+            // Ma_lop
+            // 
+            this.Ma_lop.Location = new System.Drawing.Point(116, 98);
+            this.Ma_lop.Multiline = true;
+            this.Ma_lop.Name = "Ma_lop";
+            this.Ma_lop.Size = new System.Drawing.Size(250, 27);
+            this.Ma_lop.TabIndex = 7;
+            this.Ma_lop.TextChanged += new System.EventHandler(this.Ma_lop_TextChanged);
+            // 
+            // Ten_sinh_vien
+            // 
+            this.Ten_sinh_vien.Location = new System.Drawing.Point(116, 65);
+            this.Ten_sinh_vien.Multiline = true;
+            this.Ten_sinh_vien.Name = "Ten_sinh_vien";
+            this.Ten_sinh_vien.Size = new System.Drawing.Size(250, 27);
+            this.Ten_sinh_vien.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ten_lop";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ma_lop";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ten_sinh_vien";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // MSSV
+            // 
+            this.MSSV.Location = new System.Drawing.Point(116, 34);
+            this.MSSV.Multiline = true;
+            this.MSSV.Name = "MSSV";
+            this.MSSV.Size = new System.Drawing.Size(250, 25);
+            this.MSSV.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MSSV";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(757, 174);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(786, 419);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 448);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MSSV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Ten_lop;
+        private System.Windows.Forms.TextBox Ma_lop;
+        private System.Windows.Forms.TextBox Ten_sinh_vien;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiThiLK
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        string str = @"Data Source=MSI;Initial Catalog=C#;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+
+
+        void loaddata()
+        {
+            using (connection = new SqlConnection(str))
+            {
+                connection.Open();
+
+                // Lấy dữ liệu cho table1 và hiển thị trong dataGridView1
+                command = new SqlCommand("SELECT * FROM SINH_VIEN", connection);
+                adapter.SelectCommand = command;
+                table.Clear();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+
+
+
+                connection.Close();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(str);
+            connection.Open();
+            loaddata();
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dataGridView1.CurrentCell.RowIndex;
+            MSSV.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
+            Ten_sinh_vien.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
+            Ma_lop.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+            Ten_lop.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
+            loaddata();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (connection = new SqlConnection(str))
+                {
+                    connection.Open();
+                    command = connection.CreateCommand();
+                    command.CommandText = "INSERT INTO SINH_VIEN (MSSV,Ten_sinh_vien,Ma_lop,Ten_lop) " +
+                "VALUES (@MSSV,@Ten_sinh_vien,@Ma_lop,@Ten_lop);";
+                    command.Parameters.AddWithValue("@MSSV", MSSV.Text);
+                    command.Parameters.AddWithValue("@Ten_sinh_vien", Ten_sinh_vien.Text);
+                    command.Parameters.AddWithValue("@Ma_lop", Ma_lop.Text);
+                    command.Parameters.AddWithValue("@Ten_lop", Ten_lop.Text);
+                    command.ExecuteNonQuery();
+                    loaddata();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (connection = new SqlConnection(str))
+                {
+                    connection.Open();
+                    command = connection.CreateCommand();
+                    command.CommandText = "DELETE FROM SINH_VIEN WHERE MSSV=@MSSV";
+                    command.Parameters.AddWithValue("@MSSV", MSSV.Text);
+                    command.ExecuteNonQuery();
+                    loaddata();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("lỗi:" + "không thể xóa thông tin sinh viên", "lỗi", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(str))
+                {
+                    connection.Open();
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = "UPDATE SINH_VIEN SET " +
+                            " Ten_sinh_vien=@Ten_sinh_vien,Ma_lop=@Ma_lop,Ten_lop=@Ten_lop WHERE MSSV=@MSSV";
+                        command.Parameters.AddWithValue("@Ten_sinh_vien", Ten_sinh_vien.Text);
+                        command.Parameters.AddWithValue("@Ma_lop", Ma_lop.Text);
+                        command.Parameters.AddWithValue("@Ten_lop", Ten_lop.Text);
+                        command.Parameters.AddWithValue("@MSSV", MSSV.Text);
+                        command.ExecuteNonQuery();
+                    }
+                    loaddata();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: Không thể khởi tạo thông tin sinh viên\n" + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ma_lop_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
